f9b9c70e2551749ce1d5027ceff07a8aa2020a03
root <root@localhost.localdomain>
2019-04-18 21:50:32 +0530

    Optimize Js detection

--- kicks3.py
+++ kicks3.py
@@ -1,18 +1,17 @@
 import requests,re,argparse
 bucket=[]
 ap = argparse.ArgumentParser()
-ap.add_argument("-u", "--url", required=True,
-	help="Please enter Url start with http  or https")
+ap.add_argument("-u", "--url", required=True,help="Please enter Url start with http or https")
 args = vars(ap.parse_args())
 target=args['url']
 def remove_duplicate(x):
   return list(dict.fromkeys(x))
 try:
    html=requests.get(target,timeout=10).content
-   findjs=r"(?<=src=['\"]).+\.js"
+   findjs=r"(?<=src=['\"])[a-zA-Z0-9_\.\-\:\/]+\.js"
    js=re.findall(findjs,html)
    s3=re.findall(r"[a-zA-Z\-_0-9]+\.s3\.?(?:[a-zA-Z\-_0-9]+)?\.amazonaws\.com",html)
-   print("please  wait")
+   print("Please  Wait")
    #print(s3)
    if len(js)>0:
       for i  in js:
@@ -28,6 +27,7 @@ try:
           if s3:
              bucket=bucket+s3
 except Exception as x:
+       print x
        pass
 for b in remove_duplicate(bucket):
     print(b)
