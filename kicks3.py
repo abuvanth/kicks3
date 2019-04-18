a56254c2009941434ebcbe098f54ee124bf8635a
root <root@localhost.localdomain>
2019-04-18 21:32:26 +0530

    Upload file

---
+++ kicks3.py
@@ -0,0 +1,33 @@
+import requests,re,argparse
+bucket=[]
+ap = argparse.ArgumentParser()
+ap.add_argument("-u", "--url", required=True,
+	help="Please enter Url start with http  or https")
+args = vars(ap.parse_args())
+target=args['url']
+def remove_duplicate(x):
+  return list(dict.fromkeys(x))
+try:
+   html=requests.get(target,timeout=10).content
+   findjs=r"(?<=src=['\"]).+\.js"
+   js=re.findall(findjs,html)
+   s3=re.findall(r"[a-zA-Z\-_0-9]+\.s3\.?(?:[a-zA-Z\-_0-9]+)?\.amazonaws\.com",html)
+   print("please  wait")
+   #print(s3)
+   if len(js)>0:
+      for i  in js:
+          if i.startswith('/'):
+             jsurl=i.replace('//','http://')
+             print(jsurl)
+          elif i.startswith('http'):
+               jsurl=i
+          else:
+               jsurl=target+'/'+i
+          jsfile=requests.get(jsurl,timeout=10).content
+          s3=re.findall(r"[a-zA-Z\-_0-9]+\.s3\.?(?:[a-zA-Z\-_0-9]+)?\.amazonaws\.com",jsfile)
+          if s3:
+             bucket=bucket+s3
+except Exception as x:
+       pass
+for b in remove_duplicate(bucket):
+    print(b)
