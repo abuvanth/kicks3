ca005d6242d75372659caf0a4d78e7a6f6345b04
root <root@localhost.localdomain>
2019-04-18 22:19:30 +0530

    reduce no of lines

--- kicks3/kicks3.py
+++ kicks3/kicks3.py
@@ -8,9 +8,10 @@ def remove_duplicate(x):
   return list(dict.fromkeys(x))
 try:
    html=requests.get(target,timeout=10).content
-   findjs=r"(?<=src=['\"])[a-zA-Z0-9_\.\-\:\/]+\.js"
-   js=re.findall(findjs,html)
-   s3=re.findall(r"[a-zA-Z\-_0-9]+\.s3\.?(?:[a-zA-Z\-_0-9]+)?\.amazonaws\.com",html)
+   regjs=r"(?<=src=['\"])[a-zA-Z0-9_\.\-\:\/]+\.js"
+   regs3=r"[a-zA-Z\-_0-9]+\.s3\.?(?:[a-zA-Z\-_0-9]+)?\.amazonaws\.com"
+   js=re.findall(regjs,html)
+   s3=re.findall(regs3,html)
    print("Please  Wait")
    #print(s3)
    if len(js)>0:
@@ -23,7 +24,7 @@ try:
           else:
                jsurl=target+'/'+i
           jsfile=requests.get(jsurl,timeout=10).content
-          s3=re.findall(r"[a-zA-Z\-_0-9]+\.s3\.?(?:[a-zA-Z\-_0-9]+)?\.amazonaws\.com",jsfile)
+          s3=re.findall(regs3,jsfile)
           if s3:
              bucket=bucket+s3
 except Exception as x:
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/abuvanth/kicks3",
     packages=setuptools.find_packages(),
-    scripts=['script/kicks3.py'],
+    scripts=['kicks3/kicks3.py'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
